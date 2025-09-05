79057a433b76d0523653bfbe4db266221d721941
jaulingui <jaulingui@hotmail.com>
2025-09-05 18:36:03 -0300

    Subo DB para probar

--- PrimeraPractica/PracticaWindowsForms2/Form3.Designer.cs
+++ PrimeraPractica/PracticaWindowsForms2/Form3.Designer.cs
@@ -33,25 +33,30 @@
             this.btnCalcular = new System.Windows.Forms.Button();
             this.lblResultado = new System.Windows.Forms.Label();
             this.lblExcepciones = new System.Windows.Forms.Label();
+            this.lblExcepcionLanzada = new System.Windows.Forms.Label();
+            this.btnCalcular2 = new System.Windows.Forms.Button();
+            this.txtValorDos = new System.Windows.Forms.TextBox();
+            this.txtValorUno = new System.Windows.Forms.TextBox();
+            this.lblResultado2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtNumUno
             // 
-            this.txtNumUno.Location = new System.Drawing.Point(207, 137);
+            this.txtNumUno.Location = new System.Drawing.Point(84, 140);
             this.txtNumUno.Name = "txtNumUno";
             this.txtNumUno.Size = new System.Drawing.Size(124, 20);
             this.txtNumUno.TabIndex = 0;
             // 
             // txtNumDos
             // 
-            this.txtNumDos.Location = new System.Drawing.Point(207, 189);
+            this.txtNumDos.Location = new System.Drawing.Point(84, 192);
             this.txtNumDos.Name = "txtNumDos";
             this.txtNumDos.Size = new System.Drawing.Size(124, 20);
             this.txtNumDos.TabIndex = 1;
             // 
             // btnCalcular
             // 
-            this.btnCalcular.Location = new System.Drawing.Point(230, 237);
+            this.btnCalcular.Location = new System.Drawing.Point(107, 240);
             this.btnCalcular.Name = "btnCalcular";
             this.btnCalcular.Size = new System.Drawing.Size(75, 23);
             this.btnCalcular.TabIndex = 2;
@@ -63,7 +68,7 @@
             // 
             this.lblResultado.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.lblResultado.AutoSize = true;
-            this.lblResultado.Location = new System.Drawing.Point(207, 315);
+            this.lblResultado.Location = new System.Drawing.Point(107, 314);
             this.lblResultado.Name = "lblResultado";
             this.lblResultado.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.lblResultado.Size = new System.Drawing.Size(0, 13);
@@ -74,17 +79,67 @@
             // 
             this.lblExcepciones.AutoSize = true;
             this.lblExcepciones.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblExcepciones.Location = new System.Drawing.Point(207, 65);
+            this.lblExcepciones.Location = new System.Drawing.Point(84, 68);
             this.lblExcepciones.Name = "lblExcepciones";
             this.lblExcepciones.Size = new System.Drawing.Size(107, 22);
             this.lblExcepciones.TabIndex = 4;
             this.lblExcepciones.Text = "Excepciones";
+            // 
+            // lblExcepcionLanzada
+            // 
+            this.lblExcepcionLanzada.AutoSize = true;
+            this.lblExcepcionLanzada.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblExcepcionLanzada.Location = new System.Drawing.Point(301, 68);
+            this.lblExcepcionLanzada.Name = "lblExcepcionLanzada";
+            this.lblExcepcionLanzada.Size = new System.Drawing.Size(181, 22);
+            this.lblExcepcionLanzada.TabIndex = 8;
+            this.lblExcepcionLanzada.Text = "Excepciones lanzadas";
+            // 
+            // btnCalcular2
+            // 
+            this.btnCalcular2.Location = new System.Drawing.Point(324, 240);
+            this.btnCalcular2.Name = "btnCalcular2";
+            this.btnCalcular2.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular2.TabIndex = 7;
+            this.btnCalcular2.Text = "Calcular";
+            this.btnCalcular2.UseVisualStyleBackColor = true;
+            this.btnCalcular2.Click += new System.EventHandler(this.btnCalcular2_Click);
+            // 
+            // txtValorDos
+            // 
+            this.txtValorDos.Location = new System.Drawing.Point(301, 192);
+            this.txtValorDos.Name = "txtValorDos";
+            this.txtValorDos.Size = new System.Drawing.Size(124, 20);
+            this.txtValorDos.TabIndex = 6;
+            // 
+            // txtValorUno
+            // 
+            this.txtValorUno.Location = new System.Drawing.Point(301, 140);
+            this.txtValorUno.Name = "txtValorUno";
+            this.txtValorUno.Size = new System.Drawing.Size(124, 20);
+            this.txtValorUno.TabIndex = 5;
+            // 
+            // lblResultado2
+            // 
+            this.lblResultado2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblResultado2.AutoSize = true;
+            this.lblResultado2.Location = new System.Drawing.Point(324, 314);
+            this.lblResultado2.Name = "lblResultado2";
+            this.lblResultado2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblResultado2.Size = new System.Drawing.Size(0, 13);
+            this.lblResultado2.TabIndex = 9;
+            this.lblResultado2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(546, 537);
+            this.Controls.Add(this.lblResultado2);
+            this.Controls.Add(this.lblExcepcionLanzada);
+            this.Controls.Add(this.btnCalcular2);
+            this.Controls.Add(this.txtValorDos);
+            this.Controls.Add(this.txtValorUno);
             this.Controls.Add(this.lblExcepciones);
             this.Controls.Add(this.lblResultado);
             this.Controls.Add(this.btnCalcular);
@@ -108,5 +163,10 @@
         private System.Windows.Forms.Button btnCalcular;
         private System.Windows.Forms.Label lblResultado;
         private System.Windows.Forms.Label lblExcepciones;
+        private System.Windows.Forms.Label lblExcepcionLanzada;
+        private System.Windows.Forms.Button btnCalcular2;
+        private System.Windows.Forms.TextBox txtValorDos;
+        private System.Windows.Forms.TextBox txtValorUno;
+        private System.Windows.Forms.Label lblResultado2;
     }
 }
--- PrimeraPractica/PracticaWindowsForms2/Form3.cs
+++ PrimeraPractica/PracticaWindowsForms2/Form3.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,12 +17,11 @@ namespace PracticaWindowsForms2
         {
             InitializeComponent();
         }
-
         private void Form3_Load(object sender, EventArgs e)
         {
 
         }
-
+        //En este caso la acción del botón realiza la operación y valida la excepción
         private void btnCalcular_Click(object sender, EventArgs e)
         {
             int a, b, r;
@@ -49,7 +49,59 @@ namespace PracticaWindowsForms2
             {
                 lblResultado.Text = "ERROR: Motivo no contemplado";
             }
-                
+        }
+
+        //La interacción en el entorno visual realiza la petición de la acción.
+        //Se llama a una función que se encarga de realizar la acción y muestra el resultado, o el error
+        private void btnCalcular2_Click(object sender, EventArgs e)
+        {
+
+            int resultado;
+            try
+            {
+                resultado = calcular();
+                lblResultado2.Text = "Resultado = " + resultado;
+            }
+            catch (FormatException ex)
+            {
+                lblResultado2.Text = "ERROR: Solo cargar número";
+            }
+            catch (DivideByZeroException ex)
+            {
+                lblResultado2.Text = "ERROR: No se puede dividir por cero";
+            }
+            catch (OverflowException ex)
+            {
+                lblResultado2.Text = "ERROR: Número muy largo para mostrar";
+            }
+            catch (Exception ex)
+            {
+                lblResultado2.Text = "ERROR: Motivo no contemplado";
+            }
+            finally
+            {
+                //instrucciones que se ejecutan aunque haya errores
+                //cerrar un archivo, o conexión a base de datos
+            }
+        }
+        //Función llamada por la acción de la interfaz grafica. Realiza la operación y devuelve
+        //un valor, o un error para que sea mostrado
+        private int calcular()
+        {
+            int a, b, r;
+            try
+            {
+                a = int.Parse(txtValorUno.Text);
+                b = int.Parse(txtValorDos.Text);
+                r = a / b;
+                return r;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+
+            }
         }
     }
 }
